581a6823781f15084e4f587aff7046c8bda98773
maArami <aram@ali.localdomain>
2022-03-30 08:47:25 +0430

    build the files

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include "bst.h"
+#include <gtest/gtest.h>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    if (true) // make false to run unit-tests
+    {
+        std::cout << "Hello" << std::endl;
+
+    } else {
+        ::testing::InitGoogleTest(&argc, argv);
+        std::cout << "RUNNING TESTS ..." << std::endl;
+        int ret { RUN_ALL_TESTS() };
+        if (!ret)
+            std::cout << "<<<SUCCESS>>>" << std::endl;
+        else
+            std::cout << "FAILED" << std::endl;
+    }
+    return 0;
+}
